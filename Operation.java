e1edd16e7ddd4c08e88acf3934171c347fb6c0c5
gantagayatri <95686931+gantagayatri@users.noreply.github.com>
2022-11-10 21:36:23 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Operation{
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int a,b;
+		System.out.println("enter a and b");
+		a=in.nextInt();
+		b=in.nextInt();
+		int c=a/b;
+		int d=a*b;
+		int e=a-b;
+		int m=a+b;
+		System.out.println("after dividing:"+c);
+		System.out.println("after multiplying:"+d);
+		System.out.println("after substrct:" +e);
+		System.out.println("after addition:" +m);
+	}
+}
---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Palindrome{
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int a,r,sum=0,temp;
+		System.out.println("enter the a");
+		a=in.nextInt();
+		temp=a;
+	    int l=a%2;
+		while(a!=0){
+		r=a%10;
+		sum=(sum*10)+r;
+		a=a/10;
+		}
+		if(l==0 && temp==sum){
+			System.out.println("plaindrome");
+		}
+		else{
+			System.out.println("not plaindrome");
+		}
+	}
+}
+	
