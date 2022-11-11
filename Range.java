5a0daca788a2a853f31014baa4b3683734f03f3f
gantagayatri <95686931+gantagayatri@users.noreply.github.com>
2022-11-11 20:54:51 +0530

    Update Range.java

--- Range.java
+++ Range.java
@@ -1,3 +1,5 @@
+#Write a program to find if numbers which are divisible by 23 between 10 to 150
+	
 public class Range{
 	public static void main(String args[]){
 		int a=10,b=150;
@@ -6,5 +8,5 @@ public class Range{
 				System.out.println("\n number:" +a);
 			}
 		}
-    }
+	}
 }
