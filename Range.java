d46a9f9cb825bec6c3238900a7555951f707f60a
gantagayatri <95686931+gantagayatri@users.noreply.github.com>
2022-11-11 20:53:12 +0530

    Add files via upload

---
+++ Range.java
@@ -0,0 +1,10 @@
+public class Range{
+	public static void main(String args[]){
+		int a=10,b=150;
+		for(a=10;b>=a;a++){
+			if(a%23==0){
+				System.out.println("\n number:" +a);
+			}
+		}
+    }
+}
